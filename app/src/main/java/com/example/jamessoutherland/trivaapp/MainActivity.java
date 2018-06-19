cb02ece3546194ebb6cb019886ef30bcfe65e36b
James Southerland <james.f.southerland@gmail.com>
2018-06-19 14:17:38 -0400

    Created fragment

--- app/src/main/java/com/example/jamessoutherland/trivaapp/MainActivity.java
+++ app/src/main/java/com/example/jamessoutherland/trivaapp/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.jamessoutherland.trivaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.nio.file.attribute.FileAttribute;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    private QuestionCreatorFragment questionCreatorFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+    @OnClick(R.id.add_question_button)
+    protected void addQuestionClicked() {
+
+        questionCreatorFragment = QuestionCreatorFragment.newInstance();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, questionCreatorFragment).commit();
+
+
+    }
+
 }
