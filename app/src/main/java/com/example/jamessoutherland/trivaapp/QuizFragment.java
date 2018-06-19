2439eded4df38038905f89d894328d06290cd787
James Southerland <james.f.southerland@gmail.com>
2018-06-19 15:28:40 -0400

    Created Butter Knife

---
+++ app/src/main/java/com/example/jamessoutherland/trivaapp/QuizFragment.java
@@ -0,0 +1,22 @@
+package com.example.jamessoutherland.trivaapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public class QuizFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_quiz, container, false);
+        ButterKnife.bind(this,view); 
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
